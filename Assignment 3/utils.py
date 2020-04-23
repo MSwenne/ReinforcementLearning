16a2d69858be891c6f0976c907674bab1f2494f4
MSwenne <m.m.swenne@umail.leidenuniv.nl>
2020-04-23 18:20:37 +0200

    Update utils.py

--- Assignment 3/utils.py
+++ Assignment 3/utils.py
@@ -23,17 +23,3 @@ def get_input(message, valid, ending="\n"):
         print(message, end=ending)
         result = input()
     return result
-
-def update_scene(num, frames, patch):
-    patch.set_data(frames[num])
-    return patch
-
-def plot_animation(frames, repeat=False, interval=40):
-    fig = plt.figure()
-    patch = plt.imshow(frames[0])
-    plt.axis('off')
-    anim = animation.FuncAnimation(
-        fig, update_scene, fargs=(frames, patch),
-        frames=len(frames), repeat=repeat, interval=interval)
-    plt.close()
-    return anim
