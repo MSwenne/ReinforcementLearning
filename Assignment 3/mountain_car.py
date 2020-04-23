7f6f6255b08dd0c56dda96c5f57f9a3feb3033bb
MSwenne <m.m.swenne@umail.leidenuniv.nl>
2020-04-23 20:14:52 +0200

    Update mountain_car.py

--- Assignment 3/mountain_car.py
+++ Assignment 3/mountain_car.py
@@ -14,6 +14,7 @@
 
 import matplotlib.pyplot as plt
 import tensorflow as tf
+from matplotlib.lines import Line2D
 from tensorflow import keras
 from tqdm import tqdm
 from collections import deque
@@ -27,9 +28,18 @@ SCORE_DATA = []
 class Model:
     def __init__(self, env):
         self.env = env
+        self.batch_size = 32
+        self.tensorboard = tf.keras.callbacks.TensorBoard(
+            log_dir='./my_tf_logs',
+            histogram_freq=0,
+            batch_size=self.batch_size,
+            write_graph=True,
+            write_grads=True
+        )
+        self.iteration_loss = np.array([])
+        self.total_loss = 0.0
         self.discount_rate = 0.99
         self.max_steps = 201
-        self.batch_size = 32
         self.memory = deque(maxlen=20000)
         self.optimizer = keras.optimizers.Adam(lr=0.001)
         self.activation = 'relu'
@@ -47,6 +57,7 @@ class Model:
             keras.layers.Dense(self.output_size, activation='linear')
         ])
         model.compile(loss=self.loss, optimizer=self.optimizer)
+        self.tensorboard.set_model(model)
         return model
 
     def train(self, iterations=100):
@@ -55,12 +66,12 @@ class Model:
             rewardSum = 0
             max_position=-99
             obs = self.env.reset().reshape(1, 2)
+            all_actions = []
             for s in range(self.max_steps):
                 if i % 50 == 0:
                     self.env.render()
                 epsilon = max(1 - i * 0.05, 0.01)
                 action = self.predict(obs, epsilon)
-                # print(action, end='')
                 next_obs, reward, done, _ = self.env.step(action)
                 next_obs = next_obs.reshape(1, 2)
                 if next_obs[0][0] > max_position:
@@ -70,12 +81,18 @@ class Model:
                 self.memory.append([obs, next_obs, action, reward, done])
                 self.training_step()
                 rewardSum += reward
+                all_actions.append(action)
                 obs = next_obs
-                # if (s+1) % 50 == 0:
-                #     print()
                 if done:
-                    # print()
                     break
+            self.tensorboard.on_epoch_end(i, {
+                'game_score': rewardSum, 
+                'step': s, 
+                'all_actions': all_actions, 
+                'iteration_loss': np.mean(self.iteration_loss), 
+                'total_loss': self.total_loss 
+            })
+            self.iteration_loss = np.array([])
             if s >= 199:
                 print('Failed to finish task in iteration {}/{}'.format(i,iterations))
                 WIN_DATA.append(False)
@@ -141,22 +158,16 @@ class Model:
 if __name__ == '__main__':
     env = gym.make('MountainCar-v0')
     model = Model(env)
-    model.train(iterations=500)
-    # model.load_weights('./weights')
-    for _ in range(50):
+    # model.train(iterations=10)
+    model.load_weights('./weights')
+    for _ in range(10):
         model.play_one_game()
-    model.save_weights('./weights')
+    # model.save_weights('./weights')
     env.close()
 
     f = open('./data.txt','w+')
-    string1 = ''
-    string1 += str(WIN_DATA) + ' , '
-    string1 += '\n'
-    f.write(string1)
-    string2 = ''
-    string2 += str(SCORE_DATA) + ' , '
-    string2 += '\n'
-    f.write(string2)
+    f.write(str(WIN_DATA) + '\n')
+    f.write(str(SCORE_DATA) + '\n')
     f.close()
 
     lose = [i for i, x in enumerate(WIN_DATA) if not x]
@@ -164,21 +175,21 @@ if __name__ == '__main__':
     score_lose = [i for i, x in enumerate(SCORE_DATA) if x <= -199]
     score_win = [i for i, x in enumerate(SCORE_DATA) if x > -199]
     fig,a =  plt.subplots(2,1)
-    a[0].scatter(score_lose,[SCORE_DATA[i] for i in score_lose] , c='r')
-    a[0].scatter(score_win,[SCORE_DATA[i] for i in score_win] , c='g')
-    a[0].plot([np.mean(SCORE_DATA)]*len(SCORE_DATA))
+    a[0].scatter(score_lose,[SCORE_DATA[i] for i in score_lose] , c='r', s=6)
+    a[0].scatter(score_win,[SCORE_DATA[i] for i in score_win] , c='g', s=6)
+    a[0].plot([np.mean(SCORE_DATA)]*len(SCORE_DATA), c='b')
     a[0].set_title('Scores')
-    a[1].scatter(lose,[WIN_DATA[i] for i in lose] , c='r')
-    a[1].scatter(win,[WIN_DATA[i] for i in win] , c='g')
+    a[1].scatter(lose,[WIN_DATA[i] for i in lose] , c='r', s=6)
+    a[1].scatter(win,[WIN_DATA[i] for i in win] , c='g', s=6)
     a[1].set_title('Wins')
     plt.sca(a[0])
-    plt.xticks(range(0,len(WIN_DATA),10),range(0,len(WIN_DATA),10))
+    plt.xticks(range(0,len(WIN_DATA),50),range(0,len(WIN_DATA),50))
     plt.yticks([0.0, -200.0], ["0","-200"])
+    plt.legend([Line2D([0], [0], color='b', lw=4, label='mean')])
     plt.sca(a[1])
-    plt.xticks(range(0,len(WIN_DATA),10),range(0,len(WIN_DATA),10))
+    plt.xticks(range(0,len(WIN_DATA),50),range(0,len(WIN_DATA),50))
     plt.yticks([1.0, 0.0], ["True","False"])
     plt.ylabel('Score')
-    # plt.legend(['score', 'win'])
     fig.tight_layout(pad=3.0)
     plt.savefig('stats.png')
     plt.show()
